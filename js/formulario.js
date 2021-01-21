ca865106ab0f30137d6e59eb030e199190c06c05
sebastiangr2120 <76019181+sebastiangr2120@users.noreply.github.com>
2021-01-20 21:00:11 -0600

    Update formulario.js

--- js/formulario.js
+++ js/formulario.js
@@ -1,5 +1,4 @@
 function saluda() {
-        const respuesta = `Bienvenido ${nombre.value}, tu boleta es ${boleta.value}, tu grupo es ${grupo.value}, la materia es ${materia.value} y la fecha de hoy es ${fecha.value}.`;
-        alert(respuesta);
+        Hola bienvenido;
         
       }
