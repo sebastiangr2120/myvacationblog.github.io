d8569660e784b632ff9318f6eb185b9ba5a7f9b6
sebastiangr2120 <76019181+sebastiangr2120@users.noreply.github.com>
2021-01-22 20:59:11 -0600

    Delete mi-progreso.js

--- js/mi-progreso.js
+++
@@ -1,7 +0,0 @@
-class MiProgreso extends HTMLElement {
-  conectedCallback(){
-    this.innerHTML = /* html */
-      `<progress max="100">Cargando....</progress>`;
-  }
-}
-customElements.define(mi-progreso",MiProgreso);
